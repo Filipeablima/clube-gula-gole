5b52920d9e133bbd42f3a2f6b3f868c2108d15a9
Filipeablima <filipeablima1987@gmail.com>
2026-05-11 02:28:38 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// Cole aqui a URL do Web App publicado no Google Apps Script.
-// Exemplo: https://script.google.com/macros/s/AKfycbxxxx/exec
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby1v3-cs8p3bQ30JRDAp-_KPA00Piu1hypmCaMpvqtI9BcVxpxU0nJTojCaW1yxVv06IQ/exec";
+// URL do Web App publicado no Google Apps Script.
+// Mantenha aqui a URL que termina com /exec
+const SCRIPT_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
