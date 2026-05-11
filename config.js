d3d4c331537965c6f13251ce9d3dcc9ae85bbea9
Filipeablima <filipeablima1987@gmail.com>
2026-05-11 02:37:37 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // URL do Web App publicado no Google Apps Script.
 // Mantenha aqui a URL que termina com /exec
-const SCRIPT_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby1v3-cs8p3bQ30JRDAp-_KPA00Piu1hypmCaMpvqtI9BcVxpxU0nJTojCaW1yxVv06IQ/exec";
